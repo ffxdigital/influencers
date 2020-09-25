b1fe828d7bbcaaf653892cb78dbc1d3953fc4f13
Leonardo <lnfalotico@fcl.com.br>
2020-09-25 18:50:48 -0300

    Projeto Influencers

---
+++ core/config/config.php
@@ -0,0 +1,10 @@
+<?php
+defined('ALTUMCODE') || die();
+
+$config = [
+    'database_host'        => 'localhost',
+    'database_username'    => 'root',
+    'database_password'    => 'root',
+    'database_name'        => 'social',
+    'url'                  => 'http://localhost/'
+];
